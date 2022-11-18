b8410b6a1f1d109f8305ee15158ea5b2032a0529
Richard Stephen <richardoffama@gmail.com>
2022-11-18 17:28:08 +0000

    Added code to BasketApplication to fetch a request for an arraylist of basket...

---
+++ src/test/java/atu/ie/BasketTest.java
@@ -0,0 +1,59 @@
+package atu.ie;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasketTest {
+
+    Basket basketItem1;
+    Basket basketItem2;
+    Basket basketItem3;
+    Basket basketItem4;
+
+    ArrayList<Basket> myBasket = new ArrayList<Basket>();
+    @BeforeEach
+    void setUp() {
+        basketItem1 = new Basket("Drug",200,3);
+        basketItem2 = new Basket("charger",150,2);
+        basketItem3 = new Basket("laptop",50,6);
+        basketItem4 = new Basket("phone",10,8);
+
+        myBasket.add(basketItem1);
+        myBasket.add(basketItem2);
+        myBasket.add(basketItem3);
+        myBasket.add(basketItem4);
+
+    }
+
+    @Test
+    void testBasketSize(){
+        assertEquals(4,myBasket.size());
+    }
+
+    @Test
+    void testItemName(){
+        assertEquals("Drug", basketItem1.getItemName());
+    }
+
+    @Test
+    void testItemPrice(){
+        assertEquals(150,basketItem2.getItemPrice());
+    }
+
+    @Test
+    void testDrugsQuantity(){
+        Exception drugMsg = assertThrows(IllegalArgumentException.class, () ->{new Basket("drug",100,3).getItemName();});
+        assertEquals("Sorry but you can only buy 2 drugs at a time",drugMsg.getMessage());
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+}
