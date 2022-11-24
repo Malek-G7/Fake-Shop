31b142f5b054060cc68d538c9742928d6e140487
OISIN O ROURKE - STUDENT <G00379850@atu.ie>
2022-11-24 11:13:23 +0000

    test oisin

--- src/main/java/atu/ie/Users/dao/UserDAO.java
+++ src/main/java/atu/ie/Users/dao/UserDAO.java
@@ -10,8 +10,8 @@ import java.util.stream.Stream;
 public interface UserDAO {
     void insertUserToDB(User user);
     List<User> getAllUsersFromDB();
+
     void deleteUserByID(UUID uuid);
     Optional<User> getUserByID(UUID uuid);
-
-
     }
+
