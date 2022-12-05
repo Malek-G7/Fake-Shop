4d65d536d84d4249fc7f64a56d32c08e083777c6
OISIN O ROURKE - STUDENT <G00379850@atu.ie>
2022-12-05 12:57:38 +0000

    error fixes

--- src/main/java/atu/ie/Oisin/Payment.java
+++ src/main/java/atu/ie/Oisin/Payment.java
@@ -19,6 +19,8 @@ public class Payment {
         long num = input.nextLong();
         System.out.println("Card Number: " + num);
         input.close();
+
+        return cardNum;
     }
 
     public long getExpiryDate() {
